0dbe4572e5497f1b52c918d1b8fe00073fd32061
Joe Flasher <joseph.flasher@gmail.com>
2015-08-18 09:20:47 -0400

    Don't add meta block for certain formats

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ exports.register = function (server, options, next) {
   var content = options.content || {credit: 'response-meta'};
   var results = options.results || 'results';
   var routes = options.routes || ['*'];
+  var excludeFormats = options.excludeFormats || [];
 
   server.ext('onPreResponse', function (request, reply) {
-    if (routes.indexOf(request.route.path) !== -1 || routes[0] === '*') {
+    // Make sure route matches and we're not exclude based on format
+    if ((routes.indexOf(request.route.path) !== -1 || routes[0] === '*') &&
+      excludeFormats.indexOf(request.query.format) === -1) {
       if (_.has(request.response.source, name)) {
 
         request.response.source[name] = _.merge(request.response.source[name], content);
